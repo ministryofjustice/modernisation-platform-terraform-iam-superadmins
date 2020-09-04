a727d13b4599a897379126f1f189973f2c6a8b25
Jake Mulley <jakemulley@users.noreply.github.com>
2020-09-04 09:21:42 +0100

    Turn off IAM login profile creation

--- main.tf
+++ main.tf
@@ -44,6 +44,6 @@ module "iam_user" {
   version                       = "~> 2.0"
   name                          = "${each.key}-superadmin"
   force_destroy                 = true
-  create_iam_user_login_profile = true
+  create_iam_user_login_profile = false
   create_iam_access_key         = false
 }
