a415d64cffdcd9f081cbe6a1e92bbe772041b357
jakemulley <jakemulley@users.noreply.github.com>
2021-02-09 11:21:11 +0000

    Fix versions and add output description

--- outputs.tf
+++ outputs.tf
@@ -5,4 +5,5 @@ output "superadmin_passwords" {
     user.this_iam_user_name => user.this_iam_user_login_profile_encrypted_password
     if length(user.pgp_key) > 0
   }
+  description = "Map of users and PGP-encrypted passwords, e.g. { bob: 'abcdefg123456' }"
 }
